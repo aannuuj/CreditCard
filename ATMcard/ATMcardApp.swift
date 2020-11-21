12263db43d5b31668915dcc7bf9dfb536bc70c5a
aannuuj <hariompalkar@gmail.com>
2020-11-21 13:48:42 +0530

    Initial Commit

---
+++ ATMcard/ATMcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATMcardApp.swift
+//  ATMcard
+//
+//  Created by Hariom Palkar on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ATMcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
